430030215e82da00369e1f004498e4e9d630c13d
Tanti14 <santicostanti@gmail.com>
2024-12-11 23:51:55 -0300

    Solucionando problemas de conexion

--- server/app.js
+++ server/app.js
@@ -8,12 +8,11 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { ORIGIN_URL } from "./config.js";
 
-
 export const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ORIGIN_URL,
     credentials: true,
   })
 );
